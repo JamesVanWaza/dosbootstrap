c27baec13f7658b3dbb7fdbd046e6d07f134a3bd
JamesVanWaza <info@dosbranding.com>
2018-09-18 22:13:03 -0400

    Added the links in navbar so they can work

--- navbar.php
+++ navbar.php
@@ -1,12 +1,12 @@
 <body>
 <!--<nav class="navbar navbar-dark navbar-expand-lg navbar-light bg-dark"> -->
-<nav class="navbar navbar-light navbar-expand-lg text-sm-center" style="background-color: transparent;"> <a class="navbar-brand" href="https://dosbranding.com">DOSBRANDING</a>
+<nav class="navbar navbar-light navbar-expand-lg text-sm-center" style="background-color: transparent;"> <a class="navbar-brand" href="index.php">DOSBRANDING</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
   <div id="navbarNavDropdown" class="navbar-collapse collapse">
     <ul class="navbar-nav mr-auto">
     </ul>
     <ul class="navbar-nav">
-      <li class="nav-item"><a class="nav-link" href="#">Portfolio</a> </li>
+      <li class="nav-item"><a class="nav-link" href="portfolio.php">Portfolio</a> </li>
       <li class="nav-item"><a class="nav-link" href="#">Services</a> </li>
       <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a> </li>
     </ul>
