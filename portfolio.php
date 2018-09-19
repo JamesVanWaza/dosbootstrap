4afee897c45a152be3507f6fd989da366293406d
JamesVanWaza <info@dosbranding.com>
2018-09-18 22:37:05 -0400

    Changed Unsplash Image Names

--- portfolio.php
+++ portfolio.php
@@ -33,21 +33,21 @@ require 'navbar.php';
 
   <div class="card-deck">
     <div class="card">
-      <img class="card-img-top" src="https://picsum.photos/2560/1280?image=27" alt="Card image cap">
+      <img class="card-img-top" src="https://picsum.photos/2560/1280?image=29" alt="Card image cap">
       <div class="card-body">
         <h4 class="card-title">Project 4</h4>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
     <div class="card">
-      <img class="card-img-top" src="https://picsum.photos/2560/1280?image=28" alt="Card image cap">
+      <img class="card-img-top" src="https://picsum.photos/2560/1280?image=30" alt="Card image cap">
       <div class="card-body">
         <h4 class="card-title">Project 5</h4>
         <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
       </div>
     </div>
     <div class="card"> 
-      <img class="card-img-top" src="https://picsum.photos/2560/1280?image=29" alt="Card image cap">
+      <img class="card-img-top" src="https://picsum.photos/2560/1280?image=31" alt="Card image cap">
       <div class="card-body">
         <h4 class="card-title">Project 6</h4>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
