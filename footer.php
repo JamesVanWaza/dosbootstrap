53abce5f6c5163dad497ddcaa56b20b4cbfdb533
JamesVanWaza <info@dosbranding.com>
2019-01-16 21:23:43 -0500

    Tether JS is now PopperJS

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<!-- jQuery first, then Tether, then Bootstrap JS. -->
+<!-- jQuery first, then Popper, then Bootstrap JS. -->
 <script src="node_modules/jquery/dist/jquery.min.js"></script>
 <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
 <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
