cc456d849e108f6d79a0e6bfaba92041fa7c01de
JamesVanWaza <jgacuca@gmail.com>
2020-03-28 11:13:35 -0400

    Created index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/** Bootstrap **/
+import 'bootstrap';
+
+/** Botstrap SCSS **/
+import './scss/style.scss';
+
+//** HTML in JS **/
+document.write("<h1>Hello Webpack!</h1>");
+document.write("<button type='button' class='btn btn-primary'>Primary</button><br>");
+document.write("<div class='alert alert-primary' role='alert'>A simple primary alert—check it out!</div>");
+
+//document.write('<div class="ais-InstantSearch"><h2>InstantSearch.js e-commerce demo</h2><div class="right-panel"><div id="searchbox"></div><div id="hits"></div><div id="pagination"></div></div></div>');
+//document.write("<h6>Attention:: All this code is coming from javascript - that's preety cool.</h6>");
+//document.write('<div id="image"></div>');
