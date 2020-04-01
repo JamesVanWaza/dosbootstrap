0b3f7e5fe083e0e309a13f6c31192d33cf8c81a7
JamesVanWaza <jgacuca@gmail.com>
2020-04-01 12:36:46 -0400

    Added the Navigation

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import 'bootstrap';
 /** Botstrap SCSS **/
 import './scss/style.scss';
 
-//** HTML in JS **/
+/** HTML in JS **/
 document.write("<body id='page-top'>");
-document.write("<a class='menu-toggle rounded' href='#'><i class='fas fa-bars'></i></a><nav id='sidebar-wrapper'><ul class='sidebar-nav'><li class='sidebar-brand'><a class='js-scroll-trigger' href='#page-top'>Start Bootstrap</a></li><li class='sidebar-nav-item'><a class='js-scroll-trigger' href='#page-top'>Home</a></li><li class='sidebar-nav-item'><a class='js-scroll-trigger' href='#about'>About</a></li><li class='sidebar-nav-item'><a class='js-scroll-trigger' href='#services'>Services</a></li><li class='sidebar-nav-item'><a class='js-scroll-trigger' href='#portfolio'>Portfolio</a></li><li class='sidebar-nav-item'><a class='js-scroll-trigger' href='#contact'>Contact</a></li></ul></nav>");
+
+/** Navigation */
+document.write("<a class='menu-toggle rounded' href='#'><i class='fas fa-bars'></i></a><nav id='sidebar-wrapper'><ul class='sidebar-nav'><li class='sidebar-brand'><a class='js-scroll-trigger' href='#page-top'>DosbrandingInsertLogo</a></li><li class='sidebar-nav-item'><a class='js-scroll-trigger' href='#page-top'>Home</a></li><li class='sidebar-nav-item'><a class='js-scroll-trigger' href='#about'>About</a></li><li class='sidebar-nav-item'><a class='js-scroll-trigger' href='#services'>Services</a></li><li class='sidebar-nav-item'><a class='js-scroll-trigger' href='#portfolio'>Portfolio</a></li><li class='sidebar-nav-item'><a class='js-scroll-trigger' href='#contact'>Contact</a></li></ul></nav>");
+
+/** Header */
+document.write("<header class='masthead d-flex'><div class= 'container text-center my-auto'><h1 class='mb-1'>Stylish Portfolio</h1><h3 class='mb-5'><em>A Free Bootstrap Theme by Start Bootstrap</em></h3><a class='btn btn-primary btn-xl js-scroll-trigger' href='#about'>Find Out More</a></div><div class='overlay'></div></header>");
 document.write("</body>");
 //document.write('<h1>Hello Webpack!</h1>');
 //document.write('<button type='button' class='btn btn-primary'>Primary</button><br>');
