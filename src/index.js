c541e1eaeb8d311dba4552dcb025dc2d596a8ff4
JamesVanWaza <jgacuca@gmail.com>
2020-04-01 12:20:30 -0400

    Added the Navigation

--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ import 'bootstrap';
 import './scss/style.scss';
 
 //** HTML in JS **/
-document.write("<h1>Hello Webpack!</h1>");
-document.write("<button type='button' class='btn btn-primary'>Primary</button><br>");
-document.write("<div class='alert alert-primary' role='alert'>A simple primary alert—check it out!</div>");
-
-//document.write('<div class="ais-InstantSearch"><h2>InstantSearch.js e-commerce demo</h2><div class="right-panel"><div id="searchbox"></div><div id="hits"></div><div id="pagination"></div></div></div>');
-//document.write("<h6>Attention:: All this code is coming from javascript - that's preety cool.</h6>");
-//document.write('<div id="image"></div>');
+document.write("<body id='page-top'>");
+document.write("<a class='menu-toggle rounded' href='#'><i class='fas fa-bars'></i></a><nav id='sidebar-wrapper'><ul class='sidebar-nav'><li class='sidebar-brand'><a class='js-scroll-trigger' href='#page-top'>Start Bootstrap</a></li><li class='sidebar-nav-item'><a class='js-scroll-trigger' href='#page-top'>Home</a></li><li class='sidebar-nav-item'><a class='js-scroll-trigger' href='#about'>About</a></li><li class='sidebar-nav-item'><a class='js-scroll-trigger' href='#services'>Services</a></li><li class='sidebar-nav-item'><a class='js-scroll-trigger' href='#portfolio'>Portfolio</a></li><li class='sidebar-nav-item'><a class='js-scroll-trigger' href='#contact'>Contact</a></li></ul></nav>");
+document.write("</body>");
+//document.write('<h1>Hello Webpack!</h1>');
+//document.write('<button type='button' class='btn btn-primary'>Primary</button><br>');
+//document.write('<div class='alert alert-primary' role='alert'>A simple primary alert—check it out!</div>');
