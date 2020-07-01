4ff9413628f7e3725690820d0241c4b9e450fc1f
JamesVanWaza <jgacuca@gmail.com>
2020-07-01 07:38:23 -0400

    Removed all the php pages

--- footer.php
+++
@@ -1,6 +0,0 @@
-<!-- jQuery first, then Popper, then Bootstrap JS. -->
-<script src="node_modules/jquery/dist/jquery.min.js"></script>
-<script src="node_modules/popper.js/dist/umd/popper-utils.min.js"></script>
-<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-</body>
-</html>
