1a0b41e71772d77b7c6b25c13289e2e7470cd657
JamesVanWaza <jgacuca@gmail.com>
2021-03-18 10:52:54 -0400

    WIP on the contact pages

--- src/js/index.js
+++ src/js/index.js
@@ -11,22 +11,22 @@ import '@popperjs/core';
 import '../scss/style.scss';
 
 // Contact Form Validation
-(function() {
-    'use strict';
+// (function() {
+//     'use strict';
 
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation');
+//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//     var forms = document.querySelectorAll('.needs-validation');
 
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-        .forEach(function(form) {
-            form.addEventListener('submit', function(event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
+//     // Loop over them and prevent submission
+//     Array.prototype.slice.call(forms)
+//         .forEach(function(form) {
+//             form.addEventListener('submit', function(event) {
+//                 if (!form.checkValidity()) {
+//                     event.preventDefault();
+//                     event.stopPropagation();
+//                 }
 
-                form.classList.add('was-validated');
-            }, false);
-        });
-})();
+//                 form.classList.add('was-validated');
+//             }, false);
+//         });
+// })();
