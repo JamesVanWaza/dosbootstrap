a7821636918a7f70db5b92af5951794dfbbdee11
JamesVanWaza <jgacuca@gmail.com>
2021-11-15 08:46:02 -0500

    WIP on setting up the layout

--- src/js/index.js
+++ src/js/index.js
@@ -30,3 +30,31 @@ import '../scss/style.scss';
 //             }, false);
 //         });
 // })();
+
+/** Footer */
+document.body.onload = footer;
+
+function footer() {
+    // create a new div element
+    const footerDiv = document.createElement("footer");
+
+    // assign it a class
+    footerDiv.classList.add("footer");
+
+    // gets the current date
+    const copyright = new Date().getFullYear();
+
+    // gets the copyright symbol
+    const favicon = document.createElement("i");
+    favicon.classList.add("fas", "fa-copyright");
+
+    const text = document.createTextNode(" 2020 " + "- " + copyright);
+
+    // add the text node to the newly created div
+    footerDiv.appendChild(favicon);
+    footerDiv.appendChild(text);
+
+    // add the newly created element and its content into the DOM
+    const newDiv = document.getElementById("div");
+    document.body.insertBefore(footerDiv, newDiv);
+}
