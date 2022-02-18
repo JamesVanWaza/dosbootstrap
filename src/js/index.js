fb46f5d7384455a9c03b82c3936f99089fb26417
JamesVanWaza <jgacuca@gmail.com>
2022-02-18 00:54:01 -0500

    Uncommented out the form still a WIP

--- src/js/index.js
+++ src/js/index.js
@@ -13,26 +13,25 @@ import '../scss/style.scss';
 /** FontAwesome 6 */
 import '@fortawesome/fontawesome-free/js/all.js';
 
-// Contact Form Validation
-// (function() {
-//     'use strict';
+(function() {
+    'use strict';
 
-//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//     var forms = document.querySelectorAll('.needs-validation');
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation');
 
-//     // Loop over them and prevent submission
-//     Array.prototype.slice.call(forms)
-//         .forEach(function(form) {
-//             form.addEventListener('submit', function(event) {
-//                 if (!form.checkValidity()) {
-//                     event.preventDefault();
-//                     event.stopPropagation();
-//                 }
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function(form) {
+            form.addEventListener('submit', function(event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
 
-//                 form.classList.add('was-validated');
-//             }, false);
-//         });
-// })();
+                form.classList.add('was-validated');
+            }, false);
+        });
+})();
 
 /** Footer */
 document.body.onload = footer;
